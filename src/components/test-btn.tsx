7daee853b8ece123c3469f71e0d6af536fc8cd9b
pokycookie <0cookieboy0@gmail.com>
2025-11-04 19:46:16 +0900

    init: shadcn/ui

--- src/components/test-btn.tsx
+++ src/components/test-btn.tsx
@@ -1,14 +1,8 @@
 import { useState } from 'react'
+import { Button } from './ui/button'
 
 export function TestBtn() {
   const [count, setCount] = useState(0)
 
-  return (
-    <button
-      className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 border"
-      onClick={() => setCount(count + 1)}
-    >
-      Click me {count}
-    </button>
-  )
+  return <Button onClick={() => setCount(count + 1)}>Click me {count}</Button>
 }
