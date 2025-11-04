692f225abb6d2851cbed245fa36b504d1e6057ca
pokycookie <0cookieboy0@gmail.com>
2025-11-04 19:41:23 +0900

    init: tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
-});
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': new URL('./src', import.meta.url).pathname,
+      },
+    },
+  },
+})
--- src/components/test-btn.tsx
+++ src/components/test-btn.tsx
@@ -1,7 +1,14 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 export function TestBtn() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
-  return <button onClick={() => setCount(count + 1)}>Click me {count}</button>;
+  return (
+    <button
+      className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 border"
+      onClick={() => setCount(count + 1)}
+    >
+      Click me {count}
+    </button>
+  )
 }
