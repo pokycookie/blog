c48e828a795e9a18beb1d7cd53b557ecfe79bdc8
황세웅 <0cookieboy0@gmail.com>
2025-11-05 17:17:09 +0900

    [BLG] 카테고리별 보기 (#3)
    * feat: 포스트 전체보기 페이지
    * feat: 전체보기 페이지네이션
    * design: 인라인 코드, 굵게 스타일 변경
    * feat: 카테고리별 분류
    * fix: 카테고리별 baseUrl 수정

---
+++ src/components/category-selector.tsx
@@ -0,0 +1,48 @@
+import { cn } from '@/lib/utils'
+import { Button } from './ui/button'
+import type { ReactNode } from 'react'
+
+type CategorySelectorProps = {
+  currentCategory: string | null
+  categories: string[]
+  className?: string
+}
+
+export function CategorySelector({
+  currentCategory,
+  categories,
+  className,
+}: CategorySelectorProps) {
+  return (
+    <div className={cn('flex gap-2 w-full bg-background p-4 border-b border-muted', className)}>
+      <CategoryButton isActive={currentCategory === null} href="/1">
+        전체
+      </CategoryButton>
+      {categories.map((category) => (
+        <CategoryButton
+          key={category}
+          isActive={category === currentCategory}
+          href={`/${category}/1`}
+        >
+          {category.toUpperCase()}
+        </CategoryButton>
+      ))}
+    </div>
+  )
+}
+
+type CategoryButtonProps = {
+  children: ReactNode
+  isActive: boolean
+  href: string
+}
+
+function CategoryButton({ children, isActive, href }: CategoryButtonProps) {
+  return (
+    <a href={href}>
+      <Button variant="ghost" className={cn(isActive && 'bg-accent', 'cursor-pointer')}>
+        {children}
+      </Button>
+    </a>
+  )
+}
--- src/components/test-btn.tsx
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react'
-import { Button } from './ui/button'
-
-export function TestBtn() {
-  const [count, setCount] = useState(0)
-
-  return <Button onClick={() => setCount(count + 1)}>Click me {count}</Button>
-}
