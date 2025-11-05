aeebfbf68b9e8628a0eb058bd3588106a4b77b1b
pokycookie <0cookieboy0@gmail.com>
2025-11-05 19:02:45 +0900

    chore: github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,13 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwindcss from '@tailwindcss/vite'
 
+const site = process.env.SITE || 'https://pokycookie.github.io'
+const base = process.env.BASE_URL || ''
+
 // https://astro.build/config
 export default defineConfig({
+  site,
+  base,
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
