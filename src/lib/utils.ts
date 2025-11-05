26ace80fb288d828d508a676d4643f3b06c1da80
pokycookie <0cookieboy0@gmail.com>
2025-11-05 20:02:04 +0900

    fix: baseUrl 슬래시 중복 버그 수정

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,5 +7,8 @@ export function cn(...inputs: ClassValue[]) {
 
 export function url(path: string) {
   const base = import.meta.env.BASE_URL || ''
-  return `${base}${path}`
+
+  const cleanBase = base.endsWith('/') ? base.slice(0, -1) : base
+  const cleanPath = path.startsWith('/') ? path : `/${path}`
+  return `${cleanBase}${cleanPath}`
 }
